84bd35e67ab23e0923e6287fe5e213cc2322c2e3
Aledisc <alejandrodiscua9@gmail.com>
2026-03-11 18:07:16 -0600

    ae

---
+++ CollectorLibrary-API/migrations/20260303000749-create-books.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      book_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      author_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Authors',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      genre:  {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      ISBN: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      page_count: {
+        type: Sequelize.INTEGER,
+      }
+
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
